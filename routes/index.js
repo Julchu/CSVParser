9017148cfbc4df1180d973918c326bf407fd3997
Julian <jchumtl@gmail.com>
2020-09-16 23:27:47 -0400

    Modularizing code

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
+"use strict";
+
 let express = require('express');
 let router = express.Router();
 let csv = require('csv-parser');
 let fs = require('fs');
+let { getJSONInfo, getCSVInfo } = require("../services/fileServices");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	let data = fs.readFileSync('productInfo.json');
+	// let productInfo = getJSONInfo("productInfo.json");
+
+	let data = fs.readFileSync("productInfo.json", "utf8", (error, data) => {
+		JSON.parse(data);
+	});
 	
 	let productInfo = JSON.parse(data);
 	
@@ -20,73 +27,64 @@ router.post("/", async (req, res, next) => {
 	// Sets path of uploaded files
 	let path = __dirname + "/../public/uploads/";
 
-	fs.readdir(path, async function(err, fileNames) {
-		if (err) {
-			console.log("Error checking if directory is empty")
-		} else {
-			if (!fileNames.length) {
-				console.log("Directory is empty");
-			} else {
-				let productInfo = {};
+	getCSVInfo(path);
 
-				// Reads files in directory	
-				fileNames.forEach(fileName => {
-					// Reads CSV file
-					fs.createReadStream(path + fileName).pipe(csv())
-					// Accessing data
-					.on('data', async (row) => {
-						// Processing "Products" data
-						let productData = row["Products"].split("\n");
+	// fs.readdir(path, async function(err, fileNames) {
+	// 	if (err) {
+	// 		console.log("Error checking if directory is empty")
+	// 	} else {
+	// 		if (!fileNames.length) {
+	// 			console.log("Directory is empty");
+	// 		} else {
+	// 			let productInfo = {};
+
+	// 			// Reads files in directory	
+	// 			fileNames.forEach(fileName => {
+	// 				// Reads CSV file
+	// 				fs.createReadStream(path + fileName).pipe(csv())
+	// 				// Accessing data
+	// 				.on('data', async (row) => {
+	// 					// Processing "Products" data
+	// 					let productData = row["Products"].split("\n");
 						
-						for await (eachProduct of productData) {
-							if (eachProduct) {
-								eachProduct = eachProduct.split(" x ");
+	// 					for await (let eachProduct of productData) {
+	// 						if (eachProduct) {
+	// 							eachProduct = eachProduct.split(" x ");
 								
-								if (productInfo[eachProduct[1]]) {
-									productInfo[eachProduct[1]] += parseInt(eachProduct[0]);
-								} else {
-									productInfo[eachProduct[1]] = parseInt(eachProduct[0]);
-								}		
-							}
+	// 							if (productInfo[eachProduct[1]]) {
+	// 								productInfo[eachProduct[1]] += parseInt(eachProduct[0]);
+	// 							} else {
+	// 								productInfo[eachProduct[1]] = parseInt(eachProduct[0]);
+	// 							}		
+	// 						}
 												
-						}
+	// 					}
 
-						// Writes to JSON file
-						fs.writeFileSync('productInfo.json', JSON.stringify(productInfo), 'utf8', (err) => {
-							if (err) {
-								console.log(err);
-							}
-						});
-					})
+	// 					// Writes to JSON file
+	// 					fs.writeFileSync('productInfo.json', JSON.stringify(productInfo), 'utf8', (err) => {
+	// 						if (err) {
+	// 							console.log(err);
+	// 						}
+	// 					});
+	// 				})
 
-					// After processing data
-					.on('end', () => {
-						console.log('CSV file successfully processed');
-					});
+	// 				// After processing data
+	// 				.on('end', () => {
+	// 					console.log('CSV file successfully processed');
+	// 				});
 					
-					// Deletes file
-					try {
-						fs.unlinkSync(path + fileName)
-					} catch (err) {
-						// Error deleting file
-						console.error(err);
-					}
-				});
-			}
-		}
-	});
+	// 				// Deletes file
+	// 				try {
+	// 					fs.unlinkSync(path + fileName)
+	// 				} catch (err) {
+	// 					// Error deleting file
+	// 					console.error(err);
+	// 				}
+	// 			});
+	// 		}
+	// 	}
+	// });
 	res.redirect('/');
 });
 
 module.exports = router;
-
-// router.get('/', async (req, res, next) => {	
-// 	let productInfo = await getJSONInfo("productInfo.json");
-// 	console.log(productInfo);
-	
-// 	if (productInfo) {
-// 		res.render('index', { title: 'Express', productInfo: productInfo});
-// 	} else {
-// 		res.render("index", { title: "Express"});
-// 	}
-// });
--- services/fileServices.js
+++ services/fileServices.js
@@ -1,27 +1,76 @@
 "use strict";
+
 let fs = require('fs');
+let csv = require('csv-parser');
 
 let getJSONInfo = (fileName) => {
-	return new Promise((resolve, reject) => {
-		fs.readFileSync(fileName, "utf8", (error, data) => {
-			resolve(JSON.parse(data))
-			reject(error);
-		});
+	let data = fs.readFileSync(fileName, "utf8", (error, data) => {
+		JSON.parse(data);
 	});
+	
+	let productInfo = JSON.parse(data);
+
+	return productInfo;
 };
 
-let getCanadaPost = postalCode => {
-	let rates = new Promise((resolve, reject) => {
-		request(
-			"https://7ywg61mqp6.execute-api.us-east-1.amazonaws.com/prod/rates/" +
-				postalCode,
-			(error, response, body) => {
-				resolve(body);
-				reject(error);
+
+let getCSVInfo = (path) => {
+	// Sets path of uploaded files
+
+	fs.readdirSync(path, async function(err, fileNames) {
+		if (err) {
+			console.log("Error checking if directory is empty")
+		} else {
+			if (!fileNames.length) {
+				console.log("Directory is empty");
+			} else {
+				let productInfo = {};
+
+				// Reads files in directory	
+				fileNames.forEach(fileName => {
+					// Reads CSV file
+					fs.createReadStream(path + fileName).pipe(csv())
+					// Accessing data
+					.on('data', async (row) => {
+						// Processing "Products" data
+						let productData = row["Products"].split("\n");
+						
+						for await (let eachProduct of productData) {
+							if (eachProduct) {
+								eachProduct = eachProduct.split(" x ");
+								
+								if (productInfo[eachProduct[1]]) {
+									productInfo[eachProduct[1]] += parseInt(eachProduct[0]);
+								} else {
+									productInfo[eachProduct[1]] = parseInt(eachProduct[0]);
+								}		
+							}				
+						}
+
+						// Writes to JSON file
+						fs.writeFileSync('productInfo.json', JSON.stringify(productInfo), 'utf8', (err) => {
+							if (err) {
+								console.log(err);
+							}
+						});
+					})
+
+					// After processing data
+					.on('end', () => {
+						console.log('CSV file successfully processed');
+					});
+					
+					// Deletes file
+					try {
+						fs.unlinkSync(path + fileName)
+					} catch (err) {
+						// Error deleting file
+						console.error(err);
+					}
+				});
 			}
-		);
+		}
 	});
-	return rates;
-};
+}
 
-module.exports = {getJSONInfo};
+module.exports = { getJSONInfo, getCSVInfo };
